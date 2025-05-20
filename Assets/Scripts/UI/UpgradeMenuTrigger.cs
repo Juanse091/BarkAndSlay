034deda004ccd27b95ac43b39674c1b8c87b8d97
TiltRex <afls2002@gmail.com>
2025-05-20 12:30:10 -0500

    UpgradeMenu_Update

---
+++ Assets/Scripts/UI/UpgradeMenuTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class UpgradeMenuTrigger : MonoBehaviour
+{
+    public UpgradeMenuManager menuManager;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // ⚠️ Si el menú ya está abierto, no hacer nada
+            if (menuManager.upgradePanel.activeSelf)
+                return;
+
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                GameObject clickedObj = hit.collider.gameObject;
+                string tag = clickedObj.tag;
+
+                if (tag == "Player" || tag == "ArcherDog" || tag == "MacheteDog")
+                {
+                    menuManager.AbrirMenuPara(tag);
+                }
+            }
+        }
+    }
+}
